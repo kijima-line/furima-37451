0005ff6a6537e32da847824a6f93ba991e30cfdc
jun <marugo2792@gmail.com>
2022-02-23 14:39:03 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :edit, :show,]
+  before_action :authenticate_user!, except: [:index, :edit, :show]
   before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
 
@@ -21,38 +21,33 @@ class ItemsController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
-
-     
     redirect_to root_path unless current_user.id == @item.user_id
-
   end
 
   def update
-    
     if @item.update(item_params)
       redirect_to item_path
-      else
-  
+    else
+
       render :edit
     end
   end
 
   private
+
   def set_item
     @item = Item.find(params[:id])
   end
+
   def item_params
     params.require(:item).permit(:title, :description, :category_id, :status_id, :buy_cost_id, :prefecture_id, :buy_day_id,
                                  :sale, :image).merge(user_id: current_user.id)
   end
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :new
-    end
-  end
 
+  def move_to_index
+    redirect_to action: :new unless user_signed_in?
+  end
 end
