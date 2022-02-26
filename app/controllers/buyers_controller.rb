20fdcb170169a670d30f0d81c42a2455fdb360e4
jun <marugo2792@gmail.com>
2022-02-26 20:48:26 +0900

    修正_4

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,5 +1,5 @@
 class BuyersController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :show]
+  before_action :authenticate_user!, only: [:index]
   before_action :set_item, only: [:index, :create]
   before_action :set_url, only: [:index,:create]
 
