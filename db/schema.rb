0c6cf307f5673218765d9cc094d73a368fbbbf5f
jun <marugo2792@gmail.com>
2022-02-28 18:15:45 +0900

    nsn設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_114158) do
+ActiveRecord::Schema.define(version: 2022_02_28_081847) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,15 @@ ActiveRecord::Schema.define(version: 2022_02_23_114158) do
     t.index ["buyer_id"], name: "index_regions_on_buyer_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -93,4 +102,5 @@ ActiveRecord::Schema.define(version: 2022_02_23_114158) do
   add_foreign_key "buyers", "users"
   add_foreign_key "items", "users"
   add_foreign_key "regions", "buyers"
+  add_foreign_key "sns_credentials", "users"
 end
